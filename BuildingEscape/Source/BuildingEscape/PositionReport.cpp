077a32db23bc885cfcf74472d6971555f24e34da
Nicholas Wilkie <io2red@hotmail.com>
2017-01-26 14:29:45 -0800

    BE11 - Getting Transforms In C++

--- BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -19,7 +19,8 @@ void UPositionReport::BeginPlay()
 	Super::BeginPlay();
 
 	FString ObjectName = GetOwner()->GetName();
-	UE_LOG(LogTemp, Warning, TEXT("Position report for %s!"), *ObjectName );
+	FString ObjectPos = GetOwner()->GetActorLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s!"), *ObjectName, *ObjectPos );
 }
 
 
