00df7c37b747213405f433abe3433c899a83cb1d
Nicholas Wilkie <io2red@hotmail.com>
2017-01-26 15:30:19 -0800

    BE12 - Laying Out Geometry

---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,39 @@
+// Copyright Nicholas Wilkie 2017
+
+#include "BuildingEscape.h"
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Find the owning actor
+	AActor* Owner = GetOwner();
+	// Create a rotator
+	FRotator NewRotation = FRotator(0.f, 60.f, 0.f);
+	// Set the door rotation
+	Owner->SetActorRotation(NewRotation);
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// ...
+}
+
